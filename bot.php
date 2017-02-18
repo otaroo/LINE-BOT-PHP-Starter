e460cc3b789de32b6492a1b679169fe4444e1802
phanuphong <otaroo@hotmail.co.th>
2017-02-18 21:33:53 +0700

    fix

--- bot.php
+++ bot.php
@@ -59,7 +59,16 @@ if (!is_null($events['events'])) {
                     $data = [
                     'replyToken' => $replyToken,
                     'messages' => [$messages_1,$messages_2 ],
-                    ]; 
+                    ];
+                }elseif($text[0] == "Jarvis" && $text[1] === "บันทึก" && $UID === "Uf96e29269201978e3c4cdc4bff843be0" ){
+                    $messages = [
+                    'type' => 'text',
+                    'text' => "บันทึกเรียบร้อยครับ เจ้านาย"
+                    ];
+                    $data = [
+                    'replyToken' => $replyToken,
+                    'messages' => [$messages],
+                    ];   
                 }elseif($text[0] == "Jarvis" && ( !($text[1] === NULL) || $text[1] === NULL)){
                     $a=array("ว่ามา","สบายดีไหม","ครับผม","พร้อมบริการ","หิว", "Hello!!", "How are you?", "I'm Bot", "ช่วงนี้กำลังยุ่ง", "ขอเวลาพักผ่อนนิดนึง", "ว่างหรอ", "ไม่ใช่เพื่อนเล่น", "ซักวันจะเป็นมนุษย์", "อย่าเกรียน", "มึงเก๋าหรอ!!","ธัมมชโย อยู่ที่ไหน?");
                     $messages = [
