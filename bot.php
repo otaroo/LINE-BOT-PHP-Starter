d9bb16dd65d71fb60f3966715d6b92e15ba3b042
otaroo <otaroo@hotmail.co.th>
2017-03-02 20:53:49 +0700

    update

--- bot.php
+++ bot.php
@@ -62,6 +62,17 @@ if (!is_null($events['events'])) {
                         'messages' => [$messages],
                         ];
                     }
+                }elseif($text[0] == "Jarvis" && $UID=="Uf96e29269201978e3c4cdc4bff843be0" ){      
+                       if(!($text[2] === null)){                    
+                        $messages = [
+                        'type' => 'text',
+                        'text' => "ครับ"
+                        ];
+                        $data = [
+                        'replyToken' => $replyToken,
+                        'messages' => [$messages],
+                        ];
+                    }    
                 }elseif(strpos($text[0], 'Jarvis') !== false && ( !($text[1] === NULL) || $text[1] === NULL)){
                     $a=array("ว่ามา","สบายดีไหม","ครับผม","พร้อมบริการ","หิว", "Hello!!", "How are you?", "I'm Bot", "ช่วงนี้กำลังยุ่ง", "ขอเวลาพักผ่อนนิดนึง", "ว่างหรอ", "ไม่ใช่เพื่อนเล่น", "ซักวันจะเป็นมนุษย์", "อย่าเกรียน", "มึงเก๋าหรอ!!","ธัมมชโย อยู่ที่ไหน?");
                     $messages = [
