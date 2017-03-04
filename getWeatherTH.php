15a17a7ab59554071c0c04f8994640f81f20fab4
otaroo <otaroo@hotmail.co.th>
2017-03-04 11:52:52 +0700

    update getWeather

--- getWeatherTH.php
+++ getWeatherTH.php
@@ -1,9 +1,11 @@
 <?php
 
-    
-$url = "http://data.tmd.go.th/api/WeatherForecastDaily/V1/";
-$xml = simplexml_load_file($url);
-$json = json_encode($xml);
-echo $json;
 
-
+$url = 'https://jarvis-e3312.firebaseio.com/data/user.json';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+$data = json_decode($result, true);
+echo $data;
