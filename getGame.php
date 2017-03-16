c6e65a9248a436673234cf0bc8d27a4749350c4e
otaroo <otaroo@hotmail.co.th>
2017-03-16 21:59:04 +0700

    update getGame

--- getGame.php
+++ getGame.php
@@ -2,7 +2,8 @@
 $url = "http://rssfeeds.sanook.com/rss/feeds/sanook/game.news.xml";
 $xml = simplexml_load_file($url);
 $json = json_encode($xml);
-str_replace("<![CDATA","",$json);
+$json =str_replace("<![CDATA","",$json);
+$json =str_replace("]]>","",$json);
 $array = json_decode($json,TRUE);
 
 echo $json;
